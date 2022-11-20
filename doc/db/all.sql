2d52de29016bafebf213f7d596f2e59de0c3f55b
lin <1362427746@qq.com>
2022-11-20 17:42:35 +0800

    数据库插件和脚本

---
+++ doc/db/all.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `wiki`.`test`;
+CREATE TABLE `wiki`.`test`(
+    `id` BIGINT NOT NULL COMMENT 'id',
+    `name` VARCHAR(50) COMMENT '名称',
+    `password` VARCHAR(50) COMMENT '密码',
+    PRIMARY KEY (`id`)
+) ENGINE=INNODB CHARSET=utf8mb4 COMMENT='测试';
