74c156a9e7095767d03d8f022a66a5b5d0857849
lin <1362427746@qq.com>
2022-11-21 10:58:59 +0800

    配置Mybatis generator

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -5,3 +5,10 @@ CREATE TABLE `wiki`.`test`(
     `password` VARCHAR(50) COMMENT '密码',
     PRIMARY KEY (`id`)
 ) ENGINE=INNODB CHARSET=utf8mb4 COMMENT='测试';
+
+DROP TABLE IF EXISTS `wiki`.`demo`;
+CREATE TABLE `wiki`.`demo`(
+                              `id` BIGINT NOT NULL COMMENT 'id',
+                              `name` VARCHAR(50) COMMENT '名称',
+                              PRIMARY KEY (`id`)
+) ENGINE=INNODB CHARSET=utf8mb4 COMMENT='演示';
