c459e722b4327b3aed564a39eb04f4539f0e3efd
lin <1362427746@qq.com>
2022-11-22 11:36:41 +0800

    工具类CopyUtil封装BeanUtils

--- src/main/java/com/lin/wiki/service/EbookService.java
+++ src/main/java/com/lin/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.lin.wiki.entity.EbookExample;
 import com.lin.wiki.mapper.EbookMapper;
 import com.lin.wiki.req.EbookReq;
 import com.lin.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.lin.wiki.utils.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,12 +23,7 @@ public class EbookService {
         criteria.andNameLike("%" + ebookReq.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+        List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
         return respList;
     }
 }
