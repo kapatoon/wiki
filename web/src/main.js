a4af2971c4409b4f76a748a0c6a5b41de5c58926
lin <1362427746@qq.com>
2022-11-25 17:33:28 +0800

    Ant Design Vue 引入

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+// import './assets/main.css'
 
-import './assets/main.css'
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.use(Antd).mount('#app');
