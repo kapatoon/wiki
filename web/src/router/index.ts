3173093c4485b86860b21ecc5eac5b05dafd2d0a
lin <1362427746@qq.com>
2022-12-04 15:35:17 +0800

    添加电子书管理页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import AdminEbooks from '../views/admin/AdminEbooks.vue'
+
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -7,14 +9,11 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Home',
     component: Home
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/admin/ebooks',
+    name: 'AdminEbooks',
+    component: AdminEbooks
+  }
 ]
 
 const router = createRouter({
---
+++ web/src/views/admin/AdminEbooks.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>电子书</h1>
+</template>
+
+<script>
+export default {
+  name: "AdminEbooks"
+}
+</script>
+
+<style scoped>
+
+</style>
