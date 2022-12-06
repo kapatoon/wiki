9cbe3dd8da6e3fe837dac3eb2cee581129ef1890
lin <1362427746@qq.com>
2022-12-06 23:45:53 +0800

    前端适配Valid，对验证未通过的请求能够显示错误Message

--- web/src/views/admin/AdminEbooks.vue
+++ web/src/views/admin/AdminEbooks.vue
@@ -69,6 +69,7 @@
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue';
+import { message } from 'ant-design-vue';
 import axios from 'axios';
 
 export default defineComponent({
@@ -134,12 +135,15 @@ export default defineComponent({
       }).then((response) =>{
         loading.value = false;
         const data = response.data;
-        ebooks.value = data.content.list;
+        if (data.success) {
+          ebooks.value = data.content.list;
 
-        // 重置分页按钮
-        pagination.value.current = params.page;
-        pagination.value.total = data.content.total;
-
+          // 重置分页按钮
+          pagination.value.current = params.page;
+          pagination.value.total = data.content.total;
+        } else {
+          message.error(data.message);
+        }
       });
     };
 
