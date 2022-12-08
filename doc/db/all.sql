4b83b5dfc7a1aeec78968e48d757ebc17fb71f47
lin <1362427746@qq.com>
2022-12-08 20:30:16 +0800

    增加文档表SQL, 并生成doc文档持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -33,3 +33,24 @@ insert into `ebook` (id, name, description) values (2, 'Vue 入门教程', '零�
 insert into `ebook` (id, name, description) values (3, 'Python 入门教程', '零基础入门 Python 开发，企业级应用开发最佳首选框架');
 insert into `ebook` (id, name, description) values (4, 'Mysql 入门教程', '零基础入门 Mysql 开发，企业级应用开发最佳首选框架');
 insert into `ebook` (id, name, description) values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
+
+# 文档表
+drop table if exists `doc`;
+create table `doc` (
+                         `id` bigint not null comment 'id',
+                         `ebood_id` bigint not null default 0 comment '电子书id',
+                         `parent` bigint not null default 0 comment '父id',
+                         `name` varchar(50) not null comment '名称',
+                         `sort` int comment '顺序',
+                         `view_count` int default 0 comment '阅读数',
+                         `vote_count` int default 0 comment '点赞数',
+                         primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='文档';
+
+insert into `doc` (id, ebood_id, parent, name, sort, view_count, vote_count) values (1, 1, 0, '文档1', 1, 0, 0);
+insert into `doc` (id, ebood_id, parent, name, sort, view_count, vote_count) values (2, 1, 1, '文档1.1', 1, 0, 0);
+insert into `doc` (id, ebood_id, parent, name, sort, view_count, vote_count) values (3, 1, 0, '文档2', 2, 0, 0);
+insert into `doc` (id, ebood_id, parent, name, sort, view_count, vote_count) values (4, 1, 3, '文档2.1', 1, 0, 0);
+insert into `doc` (id, ebood_id, parent, name, sort, view_count, vote_count) values (5, 1, 3, '文档2.2', 2, 0, 0);
+insert into `doc` (id, ebood_id, parent, name, sort, view_count, vote_count) values (6, 1, 5, '文档2.2.1', 1, 0, 0);
+
---
+++ src/main/java/com/lin/wiki/entity/Doc.java
@@ -0,0 +1,90 @@
+package com.lin.wiki.entity;
+
+public class Doc {
+    private Long id;
+
+    private Long eboodId;
+
+    private Long parent;
+
+    private String name;
+
+    private Integer sort;
+
+    private Integer viewCount;
+
+    private Integer voteCount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEboodId() {
+        return eboodId;
+    }
+
+    public void setEboodId(Long eboodId) {
+        this.eboodId = eboodId;
+    }
+
+    public Long getParent() {
+        return parent;
+    }
+
+    public void setParent(Long parent) {
+        this.parent = parent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", eboodId=").append(eboodId);
+        sb.append(", parent=").append(parent);
+        sb.append(", name=").append(name);
+        sb.append(", sort=").append(sort);
+        sb.append(", viewCount=").append(viewCount);
+        sb.append(", voteCount=").append(voteCount);
+        sb.append("]");
+        return sb.toString();
+    }
+}
