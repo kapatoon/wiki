f751c46a35bed36ab50a03733965c843f8bcdeb3
lin <1362427746@qq.com>
2022-12-08 17:51:46 +0800

    首页-Home页面, 点击分类选项(二级分类)时显示该分类下的电子书

--- src/main/java/com/lin/wiki/service/EbookService.java
+++ src/main/java/com/lin/wiki/service/EbookService.java
@@ -31,6 +31,8 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName()))
             criteria.andNameLike("%" + req.getName() + "%");
+        if(!ObjectUtils.isEmpty(req.getCategoryId2()))
+            criteria.andCategory2IdEqualTo((req.getCategoryId2()));
 
         PageHelper.startPage(req.getPage(), req.getSize());
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -81,29 +81,33 @@ export default defineComponent({
       });
     };
 
+    const handleQueryEbooks = () => {
+      axios.get("/ebook/list", {
+        params: {
+          page: 1,
+          size: 1000,
+          categoryId2: categoryId2
+        }
+      }).then((response) => {
+        const data = response.data;
+        ebooks.value = data.content.list;
+      });
+    }
+
+    let categoryId2 = 0;
     const handleClick = (value: any) => {
       // console.log("menu click", value)
       if (value.key === 'welcome') {
         isShowWelcome.value = true;
       } else {
+        categoryId2 = value.key;
         isShowWelcome.value = false;
+        handleQueryEbooks();
       }
-      // isShowWelcome.value = value.key === 'welcome';
     };
 
     onMounted(() => {
       handleQueryCategory();
-      axios.get("/ebook/list", {
-        params: {
-          page: 1,
-          size: 1000
-        }
-      }).then((response) => {
-        const data = response.data;
-        ebooks.value = data.content.list;
-
-        console.log(response)
-      })
     })
 
     return{
