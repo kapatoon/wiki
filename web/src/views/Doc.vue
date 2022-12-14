2e76e10a3bcc92fb4ddf2c8247ceb3bf7ada87f0
lin <1362427746@qq.com>
2022-12-14 17:52:36 +0800

    使用树形组件展示文档树
     tips: 遗留 bug,文档树应当只能查询当前电子书

--- web/src/views/Doc.vue
+++ web/src/views/Doc.vue
@@ -1,8 +1,92 @@
 <template>
   <a-layout>
     <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-      <h1>文档页面</h1>
+      <a-row>
+        <a-col :span="6">
+          <a-tree
+              v-if="level1.length > 0"
+              :tree-data="level1"
+              @select="onSelect"
+              :replaceFields="{title: 'name', key: 'id', value: 'id'}"
+              :defaultExpandAll="true"
+          >
+          </a-tree>
+        </a-col>
+        <a-col :span="18">
+        </a-col>
+      </a-row>
     </a-layout-content>
   </a-layout>
 </template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref, createVNode } from 'vue';
+import axios from 'axios';
+import {message} from 'ant-design-vue';
+import {Tool} from "@/utils/tool";
+import {useRoute} from "vue-router";
+
+export default defineComponent({
+  name: 'Doc',
+  setup() {
+    const route = useRoute();
+    const docs = ref();
+    const html = ref();
+
+    /**
+     * 一级文档树，children属性就是二级文档
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1 = ref(); // 一级文档树，children属性就是二级文档
+    level1.value = [];
+
+    /**
+     * 内容查询
+     **/
+    const handleQueryContent = (id: number) => {
+      axios.get("/doc/find-content/" + id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          html.value = data.content;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = () => {
+      axios.get("/doc/all/").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          docs.value = data.content;
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(docs.value, 0);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    onMounted(() => {
+      handleQuery();
+    });
+
+    return {
+      level1,
+    }
+  }
+});
+</script>
+
 
